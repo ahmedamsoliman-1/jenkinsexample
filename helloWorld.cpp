38e8cce03c03f92ff858a20c9835bb9c400aa8f2
Ahmed Soliman <ahmed-3010@hotmail.com>
2023-05-14 14:02:26 +0400

    iostream

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,5 +1,12 @@
 #include "helloWorld.h"
+#include <iostream>
 
+
+
+
+std::string Log(x){
+    std::cout << x;
+}
 std::string HelloWorld::helloWorld() const {
     return std::string("Hello World!");
 }
